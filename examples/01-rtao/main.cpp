53c97ba9d60a07dda3042fe6d21ee621caae82d4
Guillaume Boissé <gboisse@users.noreply.github.com>
2023-01-01 00:06:33 +0100

    Happy new year!

--- examples/01-rtao/main.cpp
+++ examples/01-rtao/main.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 MIT License
 
-Copyright (c) 2022 Guillaume Boissé
+Copyright (c) 2023 Guillaume Boissé
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
