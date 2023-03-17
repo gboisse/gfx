fe5e95424bbccafcd8d0c422f21c572b5eb1ae44
Guillaume Boissé <gboisse@users.noreply.github.com>
2023-03-17 14:00:17 +0100

    Increased max. name length

--- gfx_core.h
+++ gfx_core.h
@@ -52,7 +52,7 @@ enum GfxConstant
     kGfxConstant_BackBufferCount  = 3,
     kGfxConstant_MaxRenderTarget  = 8,
     kGfxConstant_MaxAnisotropy    = 8,
-    kGfxConstant_MaxNameLength    = 32,
+    kGfxConstant_MaxNameLength    = 64,
     kGfxConstant_NumBindlessSlots = 1024
 };
 
