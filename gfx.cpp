e01b6d2a984d61fb6e1b849ffe66ea05338ece2e
Matthew Oliver <Matthew.Oliver@amd.com>
2024-12-18 21:26:15 +1100

    Add bug crash dxc options.

--- gfx.cpp
+++ gfx.cpp
@@ -8882,6 +8882,10 @@ private:
         shader_args.push_back(L"-I"); shader_args.push_back(L".");
         shader_args.push_back(L"-T"); shader_args.push_back(wshader_profile.data());
         shader_args.push_back(L"-HV 2021");
+        shader_args.push_back(L"-Wno-parameter-usage");
+        shader_args.push_back(L"-Wno-uninitialized");
+        shader_args.push_back(L"-Wno-conditional-uninitialized");
+        shader_args.push_back(L"-Wno-sometimes-uninitialized");
         if(experimental_shaders_)
         {
             shader_args.push_back(DXC_ARG_SKIP_VALIDATION);
