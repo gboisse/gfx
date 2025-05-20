34a5eed551511efe61c51b29f6721e684362705c
gboisse <gboisse@hotmail.fr>
2025-05-20 08:29:01 +0200

    Fix 01-rtao example build

--- examples/01-rtao/main.cpp
+++ examples/01-rtao/main.cpp
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ****************************************************************************/
+#include "gfx_internal_types.h"
 #include "gfx_window.h"
 #include "gfx_scene.h"
 #include "gfx_imgui.h"
